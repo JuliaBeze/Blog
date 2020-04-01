6b09caa555033705fdd910b8e759f3e4f57a2bce
JuliaBeze <juliazhorova@rambler.ru>
2020-04-01 18:05:21 +0300

    other folders

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from "./components/Header";
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <Header/>
+
+            </div>
+
+        );
+    }
+}
+
+export default App;
